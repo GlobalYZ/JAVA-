03e5fe2ac25785588abab4025287359c72798036
GlobalYZ <GlobalYZ@139.com>
2021-03-04 22:49:17 +0800

    first commit

---
+++ Contacts/src/com/atguigu/contact/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.atguigu.contact;
+
+/**
+ * 
+ * @Description 这是我的第一个程序
+ * @author GlobalYZ  Email:GlobalYZ@139.com
+ * @version v1.0
+ * @date 2021年3月4日下午3:09:43
+ *
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("helloworld!");
+	}
+}
